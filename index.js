0153ed4005f269b11dfe1eb3481ba20a6df7899a
Sam Chillcott <sam@samchillcott.com>
2020-08-30 15:00:49 +0100

    link js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function post() {
-	return {
-		status: 200,
-	};
+	fetch("https://jsonplaceholder.typicode.com/posts", {
+		method: "POST",
+		body: JSON.stringify({
+			title: "foo",
+			body: "bar",
+			userId: 1,
+		}),
+		headers: {
+			"Content-type": "application/json; charset=UTF-8",
+		},
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+	// return {
+	// 	status: 200,
+	// };
 }
+
+post();
 
 exports.post = post;
