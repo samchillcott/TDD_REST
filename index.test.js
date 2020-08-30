b6482c3adce7a5d392578dd12c5ec80e9c97efa2
Sam Chillcott <sam@samchillcott.com>
2020-08-30 12:52:41 +0100

    test 1 pass code

--- index.test.js
+++ index.test.js
@@ -1,19 +1,25 @@
+const post = require("./index");
+const get = require("./index");
+const put = require("./index");
+const patch = require("./index");
+const del = require("./index");
+
 // POST
 
+describe("post function", () => {
+	test("post function exists", () => {
+		expect(post()).toBeTruthy();
+	});
 
+	test("post function returns something?", () => {
+		expect(post()).toBeTruthy();
+	});
+});
 
 // GET
 
-
-
-
 // PUT
 
-
-
 // PATCH
-
-
-
 
 // DELETE
